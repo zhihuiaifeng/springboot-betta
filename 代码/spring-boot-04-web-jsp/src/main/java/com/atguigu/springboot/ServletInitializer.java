990772e8fa226dcffd6c07c47ca587cad98c93b0
zhihuiaifeng <47853625+zhihuiaifeng@users.noreply.github.com>
2019-06-30 11:06:06 +0800

    Update ServletInitializer.java

--- 代码/spring-boot-04-web-jsp/src/main/java/com/atguigu/springboot/ServletInitializer.java
+++ 代码/spring-boot-04-web-jsp/src/main/java/com/atguigu/springboot/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+        //war包方式调用springboot 的SpringBoot04WebJspApplication方法进行调用
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBoot04WebJspApplication.class);
